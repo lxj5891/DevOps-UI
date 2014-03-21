058b5d951d1f43581823edaaf332fbae3f68fcd2
sl_yang <shengli1222@163.com>
2014-03-21 14:30:45 +0800

    init

---
+++ Gruntfile.js
@@ -0,0 +1,46 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Task configuration.
+    clean: {
+      dist: ["dist"]
+    },
+
+    // Less
+    less: {
+      development: {
+        options: {
+          paths: ["less"]
+        },
+        files: {
+          "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
+        }
+      },
+      production: {
+        options: {
+          cleancss: true
+        },
+        files: {
+          "dist/css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
+        }
+      }
+    },
+
+    // Run predefined tasks whenever watched file patterns are added, changed or deleted.
+    watch: {
+      less: {
+          files: ["*.less", "less/*.less"]
+        , tasks: ["less:development"]
+        , options : {
+            livereload: true
+          , nospawn: true
+        }
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt, {scope: 'devDependencies'});
+};
